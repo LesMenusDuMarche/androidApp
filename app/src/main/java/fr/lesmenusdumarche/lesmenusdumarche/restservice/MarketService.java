1e4762cfb7a09b018d00409e92a184fdd8414ca6
Maxime Lebastard <lebastardmaxime@gmail.com>
2016-03-17 09:48:56 +0100

    Add retrofit and create first REST service

---
+++ app/src/main/java/fr/lesmenusdumarche/lesmenusdumarche/restservice/MarketService.java
@@ -0,0 +1,16 @@
+package fr.lesmenusdumarche.lesmenusdumarche.restservice;
+
+import java.util.List;
+
+import fr.lesmenusdumarche.lesmenusdumarche.domain.Market;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by maxime on 17/03/2016.
+ */
+public interface MarketService {
+    @GET("market/")
+    Call<List<Market>> list();
+}
