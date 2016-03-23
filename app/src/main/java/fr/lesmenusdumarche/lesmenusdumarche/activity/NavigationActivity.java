e19fe9e96983d0c24d1cc481225bc269c1b886e5
Maxime Lebastard <lebastardmaxime@gmail.com>
2016-03-23 12:51:37 +0100

    Add GPS location

--- app/src/main/java/fr/lesmenusdumarche/lesmenusdumarche/activity/NavigationActivity.java
+++ app/src/main/java/fr/lesmenusdumarche/lesmenusdumarche/activity/NavigationActivity.java
@@ -1,6 +1,13 @@
 package fr.lesmenusdumarche.lesmenusdumarche.activity;
 
+import android.Manifest;
 import android.app.FragmentManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 
@@ -10,13 +17,16 @@ import fr.lesmenusdumarche.lesmenusdumarche.fragment.CheckListFragment;
 import fr.lesmenusdumarche.lesmenusdumarche.fragment.MapFragment;
 
 import android.os.Parcelable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class NavigationActivity extends FragmentActivity {
+public class NavigationActivity extends FragmentActivity implements LocationListener {
 
     // Extra keys
     public final static String EXTRA_RECEIPES = "RECEIPES";
@@ -29,6 +39,8 @@ public class NavigationActivity extends FragmentActivity {
     protected List<Recipe> recipes = new ArrayList<>();
 
     // Sensors
+    protected LocationManager locationManager;
+    protected String provider;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,5 +56,46 @@ public class NavigationActivity extends FragmentActivity {
 
         // Sensors
         // TODO: Get GPS location and pass it to map
+        configureLocationListener();
+
+    }
+
+    private void configureLocationListener() {
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        int lat = (int) (location.getLatitude());
+        int lng = (int) (location.getLongitude());
+        Log.i("NAV","LAT : " + lat + " ,, LNG : " + lng);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
     }
 }
