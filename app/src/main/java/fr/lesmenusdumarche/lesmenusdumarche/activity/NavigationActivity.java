80c2515ebd5f0cba933e96f116d446c2c9a64ec7
Maxime Lebastard <lebastardmaxime@gmail.com>
2016-03-23 16:15:03 +0100

    Add maps, refactor fragment...

--- app/src/main/java/fr/lesmenusdumarche/lesmenusdumarche/activity/NavigationActivity.java
+++ app/src/main/java/fr/lesmenusdumarche/lesmenusdumarche/activity/NavigationActivity.java
@@ -12,9 +12,16 @@ import android.os.Bundle;
 
 
 import fr.lesmenusdumarche.lesmenusdumarche.R;
+import fr.lesmenusdumarche.lesmenusdumarche.domain.NavigationStep;
 import fr.lesmenusdumarche.lesmenusdumarche.domain.Recipe;
 import fr.lesmenusdumarche.lesmenusdumarche.fragment.CheckListFragment;
 import fr.lesmenusdumarche.lesmenusdumarche.fragment.MapFragment;
+import fr.lesmenusdumarche.lesmenusdumarche.restservice.NavigationStepService;
+import fr.lesmenusdumarche.lesmenusdumarche.restservice.RestManager;
+import io.nlopez.smartlocation.OnLocationUpdatedListener;
+import io.nlopez.smartlocation.SmartLocation;
+import retrofit2.Call;
+import retrofit2.Response;
 
 import android.os.Parcelable;
 import android.support.v4.app.ActivityCompat;
@@ -26,7 +33,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NavigationActivity extends FragmentActivity implements LocationListener {
+public class NavigationActivity extends FragmentActivity {
 
     // Extra keys
     public final static String EXTRA_RECEIPES = "RECEIPES";
@@ -37,10 +44,8 @@ public class NavigationActivity extends FragmentActivity implements LocationList
 
     // Data
     protected List<Recipe> recipes = new ArrayList<>();
-
-    // Sensors
-    protected LocationManager locationManager;
-    protected String provider;
+    protected List<NavigationStep> steps = new ArrayList<>();
+    protected NavigationStepService navigationStepService;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,51 +56,54 @@ public class NavigationActivity extends FragmentActivity implements LocationList
         mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
         checkListFragment = (CheckListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_checklist);
 
+        // Initialize REST service
+        navigationStepService = RestManager.getNavigationStepService();
+
         // Get extras (receipes)
         recipes = getIntent().getParcelableArrayListExtra(EXTRA_RECEIPES);
 
-        // Sensors
-        // TODO: Get GPS location and pass it to map
-        configureLocationListener();
+        // Make request to get points
+        Call<List<NavigationStep>> navigationStepsCall = navigationStepService.list(recipes);
+        navigationStepsCall.enqueue(new NavigationStepsCallback());
 
+        // Sensors
+        configureLocationListener();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SmartLocation.with(this).location().stop();
     }
 
     private void configureLocationListener() {
-        // Get the location manager
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        // Define the criteria how to select the locatioin provider -> use
-        // default
-        Criteria criteria = new Criteria();
-        provider = locationManager.getBestProvider(criteria, false);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+
+        SmartLocation.with(this).location()
+                        .start(new OnLocationUpdatedListener());
+    }
+
+    private void loadDataInFragments(List<NavigationStep> navigationSteps) {
+        mapFragment.setNavigationSteps(navigationSteps);
+    }
+
+    private class OnLocationUpdatedListener implements io.nlopez.smartlocation.OnLocationUpdatedListener {
+
+        @Override
+        public void onLocationUpdated(Location location) {
+            Log.i("NAV","Location updated " + location.toString());
         }
-        Location location = locationManager.getLastKnownLocation(provider);
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        int lat = (int) (location.getLatitude());
-        int lng = (int) (location.getLongitude());
-        Log.i("NAV","LAT : " + lat + " ,, LNG : " + lng);
-    }
+    private class NavigationStepsCallback implements retrofit2.Callback<List<NavigationStep>> {
+        @Override
+        public void onResponse(Call<List<NavigationStep>> call, Response<List<NavigationStep>> response) {
+            Log.i("NAV", "RESP");
+            loadDataInFragments(response.body());
+        }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        Toast.makeText(this, "Enabled new provider " + provider,
-                Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        Toast.makeText(this, "Disabled provider " + provider,
-                Toast.LENGTH_SHORT).show();
+        @Override
+        public void onFailure(Call<List<NavigationStep>> call, Throwable t) {
+            Log.i("NAV","FAIL");
+        }
     }
 }
--- app/src/main/java/fr/lesmenusdumarche/lesmenusdumarche/fragment/MapFragment.java
+++ app/src/main/java/fr/lesmenusdumarche/lesmenusdumarche/fragment/MapFragment.java
@@ -5,17 +5,93 @@ package fr.lesmenusdumarche.lesmenusdumarche.fragment;
  */
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import fr.lesmenusdumarche.lesmenusdumarche.R;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.overlays.MapEventsOverlay;
+import org.osmdroid.bonuspack.overlays.MapEventsReceiver;
+import org.osmdroid.bonuspack.overlays.Marker;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
-public class MapFragment extends Fragment {
+import java.util.List;
+
+import fr.lesmenusdumarche.lesmenusdumarche.R;
+import fr.lesmenusdumarche.lesmenusdumarche.domain.NavigationStep;
+
+public class MapFragment extends Fragment implements MapEventsReceiver {
+
+    // UI Components
+    View fragment;
+    MapView map;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        fragment = inflater.inflate(R.layout.fragment_map, container, false);
+
+        // UI Components
+        map = (MapView) fragment.findViewById(R.id.map);
+
+        // Init map
+        initializeMap();
+
+        return fragment;
+    }
+
+    protected void initializeMapOverlay() {
+        // Overlay
+        MapEventsOverlay mapEventsOverlay = new MapEventsOverlay(this.getActivity(), this);
+        map.getOverlays().add(0, mapEventsOverlay);
+    }
+
+    protected void initializeMap() {
+        map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);    // Set source (OSM, GMaps, MapBox...)
+        map.setTilesScaledToDpi(true);                  // Scale to DPI, for large screens like tablets
+
+        map.setBuiltInZoomControls(true);               // Enable zoom buttons
+        map.setMultiTouchControls(true);                // Enable pinch to zoom, rotate...
+
+        IMapController mapController = map.getController(); // Create a map controller
+        GeoPoint startPoint = new GeoPoint(48.115251, -1.638668);// Create a point
+        mapController.setCenter(startPoint);                // Put the map center on the point
+
+        mapController.setZoom(18);                          // Set an initial amount of zoom, else we will see an "earth mosaic"
+
+        initializeMapOverlay();
+
+        map.invalidate(); // We need that to make OSMDroid to reload the map
+    }
+
+    public void setNavigationSteps(List<NavigationStep> steps) {
+        map.getOverlays().clear();
+        initializeMapOverlay();
+
+        for(NavigationStep ns : steps) {
+            Marker marker = new Marker(map);
+
+            marker.setPosition(new GeoPoint(ns.getLatitude(), ns.getLongitude()));
+            marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+
+            //marker.setIcon(ContextCompat.getDrawable(this, R.drawable.droneicon));
+            marker.setTitle(ns.getSeller().getName());
+
+            map.getOverlays().add(marker);
+        }
+    }
+
+    @Override
+    public boolean singleTapConfirmedHelper(GeoPoint p) {
+        return false;
+    }
+
+    @Override
+    public boolean longPressHelper(GeoPoint p) {
+        return false;
     }
 }
